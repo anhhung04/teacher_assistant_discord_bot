f837fc0cf530fbc495e818c584bdcb8aeaf6b72d
anhhunglhp2904 <hoanganhhung29042004@gmail.com>
2022-07-03 16:08:28 +0700

    fix add guild

--- messageCommands/checkAndCreateGuild.js
+++ messageCommands/checkAndCreateGuild.js
@@ -8,7 +8,7 @@ module.exports = {
         if(guildDB){
             return message.reply('Thông tin của server này đã có trong hệ thống');
         }else{
-            await guildDB.create({guildId: message.guildId});
+            await guildModel.create({guildId: message.guildId});
             return message.reply('Đã thêm thông in về server này vào hệ thống, vui lòng cài đặt các kênh để sử dụng');
         }
     },
